2a0ba91a4f8a4b5bcf5a078e88cfa01547f0cee2
juestany <jk620@wp.pl>
2024-04-07 11:49:43 +0200

    added half of layouts

---
+++ app/src/main/java/com/example/sr13/MainActivity.kt
@@ -0,0 +1,16 @@
+
+package com.example.sr13
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+}
