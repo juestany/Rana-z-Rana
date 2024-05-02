586bcbc51f09c440ad3c0e23473d63a2f05d568f
rafalowiczagata <rafalowiczagata@gmail.com>
2024-05-02 19:44:18 +0200

    Implements firestore and login activity

--- app/src/main/java/com/example/sr13/LoginActivity.kt
+++ app/src/main/java/com/example/sr13/LoginActivity.kt
@@ -1,4 +1,72 @@
 package com.example.sr13
 
-class LoginActivity {
-}
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+    class LoginActivity : BaseActivity(), View.OnClickListener {
+        private var inputEmail: EditText? = null
+        private var inputPassword: EditText? = null
+        private var loginButton: Button? = null
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.login)
+            inputEmail = findViewById(R.id.loginEmail)
+            inputPassword = findViewById(R.id.loginPassword)
+            loginButton = findViewById(R.id.loginBtn)
+            loginButton?.setOnClickListener {
+                logInRegisteredUser()
+            }
+        }
+
+        override fun onClick(view: View?) {
+            if (view != null) {
+                when (view.id) {
+                    R.id.loginBtn -> {
+                        val intent = Intent(this, RegisterActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
+        }
+
+        private fun validateLoginDetails(): Boolean {
+            return when {
+                TextUtils.isEmpty(inputEmail?.text.toString().trim { it <= ' ' }) -> {
+                    showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+                    false
+                }
+                TextUtils.isEmpty(inputPassword?.text.toString().trim { it <= ' ' }) -> {
+                    showErrorSnackBar(resources.getString(R.string.err_msg_enter_password), true)
+                    false
+                }
+                else -> {
+                    showErrorSnackBar("Your details are valid", false)
+                    true
+                }
+            }
+        }
+
+        private fun logInRegisteredUser() {
+            if (validateLoginDetails()) {
+                val email = inputEmail?.text.toString().trim() { it <= ' ' }
+                val password = inputPassword?.text.toString().trim() { it <= ' ' }
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            showErrorSnackBar(resources.getString(R.string.login_successfull), false)
+                            finish()
+                        } else {
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+            }
+        }
+    }}
